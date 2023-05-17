38a743341ed99fecfb5ee814e5ec7057354dc7dc
Nipho Mahlangu <niphomahlangu10@gmail.com>
2023-05-17 14:52:13 +0200

    Added an Alert dialog to log out.

--- app/src/main/java/com/example/readercollect/MainActivity.java
+++ app/src/main/java/com/example/readercollect/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.readercollect;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
@@ -14,11 +16,14 @@ import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
+    AlertDialog.Builder builder;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        builder = new AlertDialog.Builder(MainActivity.this);
 
     }
 
@@ -33,10 +38,29 @@ public class MainActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         switch (item.getItemId()){
             case R.id.itemSignOut:
-                FirebaseAuth.getInstance().signOut();
-                startActivity(new Intent(MainActivity.this,Login.class));
-                Toast.makeText(MainActivity.this, "Logged out.", Toast.LENGTH_SHORT).show();
-                finish();
+                //dialog box
+                builder.setIcon(R.drawable.book);
+                builder.setTitle("Log Out");
+                builder.setMessage("Are you sure you want to log out?");
+                builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        FirebaseAuth.getInstance().signOut();
+                        startActivity(new Intent(MainActivity.this,Login.class));
+                        Toast.makeText(MainActivity.this, "Logged out.", Toast.LENGTH_SHORT).show();
+                        finish();
+                    }
+                });
+
+                builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                    }
+                });
+
+                builder.show();
+
             return true;
             default:
                 return super.onOptionsItemSelected(item);
